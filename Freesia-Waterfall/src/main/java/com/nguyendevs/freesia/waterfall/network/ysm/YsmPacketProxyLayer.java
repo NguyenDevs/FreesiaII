f75de963d70a04390594ecdb187582b483de4aca
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-03-10 05:15:55 +0700

    Update YsmPacketProxyLayer.java

--- Freesia-Waterfall/src/main/java/com/nguyendevs/freesia/waterfall/network/ysm/YsmPacketProxyLayer.java
+++ Freesia-Waterfall/src/main/java/com/nguyendevs/freesia/waterfall/network/ysm/YsmPacketProxyLayer.java
@@ -19,20 +19,6 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-/**
- * The framework which is used for all ysm packet proxies</br>
- * Note:</br>
- * <p>
- * 1.This object is only can be used for once, any duplicated entity id and
- * worker entity id updates will be ignored
- * because it cannot and should not be updated multiple times as we just use
- * them for per mappers
- * </p>
- * <p>
- * 2.Any write operations of ysm entity data is ONLY single thread safe!!!
- * </p>
- *
- */
 public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
     protected final ProxiedPlayer player;
 
@@ -46,8 +32,8 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
     private int playerEntityId = -1;
     private int workerEntityId = -1;
 
-    private int entityDataReferenceCount = 0; // Used for read-writing lock but writing is always happening on a single
-                                              // thread
+    private int entityDataReferenceCount = 0; 
+
     private YsmState lastYsmEntityData = null;
 
     private boolean proxyReady = false;
@@ -75,12 +61,7 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
         this.playerUUID = player.getUniqueId();
     }
 
-    // Read and write locks for entity data, we just use them for very, very short
-    // term operations so there is no need to
-    // worry the thread contention issue on performance
     protected void releaseWriteReference() {
-        // There is no any thread contention because the write locks are currently in
-        // our hands
         if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, -1, 0)) {
             throw new IllegalStateException("Releasing when not write-locked");
         }
@@ -95,13 +76,11 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
 
             final int curr = (int) ENTITY_DATA_REF_COUNT_HANDLE.getVolatile(this);
 
-            // Reading or another writing operations are not finished
             if (curr > 0 || curr == -1) {
                 failureCount++;
                 continue;
             }
 
-            // Another thread is acquiring write or read reference
             if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, -1)) {
                 failureCount++;
                 continue;
@@ -128,7 +107,6 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
                 throw new IllegalStateException("Setting reference count down to a value lower than 0!");
             }
 
-            // Another thread is acquiring read reference
             if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, curr - 1)) {
                 failureCount++;
                 continue;
@@ -147,13 +125,11 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
 
             final int curr = (int) ENTITY_DATA_REF_COUNT_HANDLE.getVolatile(this);
 
-            // Write locked
             if (curr == -1) {
                 failureCount++;
                 continue;
             }
 
-            // Another thread is acquiring read or write reference
             if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, curr + 1)) {
                 failureCount++;
                 continue;
@@ -162,8 +138,6 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
             break;
         }
     }
-
-    // Write and read locks end
 
     @Nullable
     @Override
@@ -188,14 +162,12 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
 
     @Override
     public void sendEntityStateTo(@NotNull ProxiedPlayer target) {
-        this.acquireReadReference(); // Acquire read reference
+        this.acquireReadReference(); 
 
         final int currEntityId = (int) PLAYER_ENTITY_ID_HANDLE.getVolatile(this);
         final YsmState currEntityData = (YsmState) LAST_YSM_ENTITY_DATA_HANDLE.getVolatile(this);
 
-        this.releaseReadReference(); // Release when we copied the value
-
-        // Not fully initialized yet
+        this.releaseReadReference();
         if (currEntityId == -1 || currEntityData == null) {
             return;
         }
@@ -210,33 +182,26 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
 
     @Override
     public void notifyFullTrackerUpdates() {
-        this.acquireReadReference(); // Acquire read reference
+        this.acquireReadReference(); 
 
         final YsmState currEntityData = (YsmState) LAST_YSM_ENTITY_DATA_HANDLE.getVolatile(this);
         final int currEntityId = (int) PLAYER_ENTITY_ID_HANDLE.getVolatile(this);
 
-        this.releaseReadReference(); // Release when we copied the value
+        this.releaseReadReference(); 
 
-        // Not fully initialized yet
         if (currEntityId == -1 || currEntityData == null) {
             return;
         }
 
-        // Prevent race condition
         if (PROXY_READY_HANDLE.compareAndSet(this, false, true)) {
-            // If we have the mapper connection
             if (this.handler != null) {
-                // Done queued tracker updates
                 this.handler.retireTrackerCallbacks();
             }
         }
 
-        // Sync to the owner self
         this.sendEntityStateToRaw(this.playerUUID, currEntityId, currEntityData);
 
-        // Fetch can-see list
         this.fetchTrackerList(this.playerUUID).whenComplete((result, ex) -> {
-            // Exception processing
             if (ex != null) {
                 Freesia.LOGGER.log(java.util.logging.Level.WARNING, "Failed to fetch tracker list for player uuid "
                         + (this.player != null ? this.player.getUniqueId() : this.playerUUID) + ": " + ex);
@@ -246,9 +211,7 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
             for (UUID toSend : result) {
                 final ProxiedPlayer queryResult = Freesia.PROXY_SERVER.getPlayer(toSend);
 
-                // If it is a real player
                 if (queryResult != null) {
-                    // Check ysm installed
                     if (Freesia.mapperManager.isPlayerInstalledYsm(toSend)) {
                         this.sendEntityStateToRaw(toSend, currEntityId, currEntityData);
                     }
@@ -270,8 +233,8 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
         final FriendlyByteBuf wrappedPacketData = new FriendlyByteBuf(Unpooled.buffer());
         wrappedPacketData.writeByte(YsmProtocolMetaFile
                 .getS2CPacketId(FreesiaConstants.YsmProtocolMetaConstants.Clientbound.MOLANG_EXECUTE));
-        wrappedPacketData.writeVarIntArray(new int[] { playerEntityId }); // Write the entity id
-        wrappedPacketData.writeUtf(expression); // Write the expression
+        wrappedPacketData.writeVarIntArray(new int[] { playerEntityId });
+        wrappedPacketData.writeUtf(expression);
 
         this.sendPluginMessageToOwner(YsmMapperPayloadManager.YSM_CHANNEL_KEY_VELOCITY, wrappedPacketData);
     }
@@ -285,8 +248,8 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
         final FriendlyByteBuf wrappedPacketData = new FriendlyByteBuf(Unpooled.buffer());
         wrappedPacketData.writeByte(YsmProtocolMetaFile
                 .getS2CPacketId(FreesiaConstants.YsmProtocolMetaConstants.Clientbound.MOLANG_EXECUTE));
-        wrappedPacketData.writeVarIntArray(entityIds); // Write the entity id
-        wrappedPacketData.writeUtf(expression); // Write the expression
+        wrappedPacketData.writeVarIntArray(entityIds);
+        wrappedPacketData.writeUtf(expression);
 
         this.sendPluginMessageToOwner(YsmMapperPayloadManager.YSM_CHANNEL_KEY_VELOCITY, wrappedPacketData);
     }
@@ -300,23 +263,15 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
             }
 
             final ProxiedPlayer receiver = queryResult;
-            final Object targetChannel = PacketEvents.getAPI().getProtocolManager().getChannel(receiver.getUniqueId()); // Get
-                                                                                                                        // the
-                                                                                                                        // netty
-                                                                                                                        // channel
-                                                                                                                        // of
-                                                                                                                        // the
-                                                                                                                        // player
-
+            final Object targetChannel = PacketEvents.getAPI().getProtocolManager().getChannel(receiver.getUniqueId()); 
             if (targetChannel == null) {
                 return;
             }
 
             final ClientVersion clientVersion = PacketEvents.getAPI().getProtocolManager()
-                    .getClientVersion(targetChannel); // Get the client version of the player
+                    .getClientVersion(targetChannel); 
 
-            final int targetProtocolVer = clientVersion.getProtocolVersion(); // Protocol version(Used for nbt
-                                                                              // remappers)
+            final int targetProtocolVer = clientVersion.getProtocolVersion(); 
             final FriendlyByteBuf wrappedPacketData = new FriendlyByteBuf(Unpooled.buffer());
 
             wrappedPacketData.writeByte(4);
@@ -343,21 +298,19 @@ public abstract class YsmPacketProxyLayer implements YsmPacketProxy {
 
     @Override
     public void setPlayerWorkerEntityId(int id) {
-        // Only update for once
         final boolean successfullyUpdated = WORKER_ENTITY_ID_HANDLE.compareAndSet(this, -1, id);
 
         if (successfullyUpdated) {
-            this.notifyFullTrackerUpdates(); // If it is the first update
+            this.notifyFullTrackerUpdates();
         }
     }
 
     @Override
     public void setPlayerEntityId(int id) {
-        // Only update for once
         final boolean successfullyUpdated = PLAYER_ENTITY_ID_HANDLE.compareAndSet(this, -1, id);
 
         if (successfullyUpdated) {
-            this.notifyFullTrackerUpdates(); // If it is the first update
+            this.notifyFullTrackerUpdates(); 
         }
     }
 
