56a769457981c3f26fb610898cbc705485bad23a
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-04-17 22:29:22 +0700

    Update build.gradle.kts

--- Freesia-Backend/build.gradle.kts
+++ Freesia-Backend/build.gradle.kts
@@ -5,9 +5,6 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     compileOnly("io.netty:netty-all:4.1.118.Final")
-    compileOnly("net.citizensnpcs:citizens-main:2.0.41-SNAPSHOT") {
-        exclude(group = "*", module = "*")
-    }
 }
 
 tasks.build {
